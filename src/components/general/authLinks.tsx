56b651b7d597c13a0b6e1f3476b6e4d37bc094c8
Ehsan <ehsan.yaghoti@yahoo.com>
2025-11-26 19:14:56 +0330

    add hover effect to links

--- src/components/general/authLinks.tsx
+++ src/components/general/authLinks.tsx
@@ -4,8 +4,8 @@ const AuthLinks = () => {
 
     return (
         <div className="flex items-center gap-2" >
-            <Link href={'#'} className=" font-medium border border-black/20 rounded-xl px-6 py-2 " >Login</Link>
-            <Link href={'#'} className="bg-primary  text-white px-6 py-2 rounded-xl font-medium hover:bg-transparent hover:text-primary hover:border " >Sign Up</Link>
+            <Link href={'#'} className=" font-medium border border-black/20 rounded-xl px-6 py-2 hover:bg-textColor hover:text-white " >Login</Link>
+            <Link href={'#'} className="bg-primary  text-white px-6 py-2 rounded-xl font-medium border border-transparent hover:bg-transparent hover:text-primary hover:border-black/20 " >Sign Up</Link>
         </div>
     )
 }
--- src/components/general/navbar.tsx
+++ src/components/general/navbar.tsx
@@ -4,10 +4,10 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center gap-10" >
-            <Link href={'#'} className="text-lg font-medium" >Products</Link>
-            <Link href={'#'} className="text-lg font-medium" >Customers</Link>
-            <Link href={'#'} className="text-lg font-medium" >Pricing</Link>
-            <Link href={'#'} className="text-lg font-medium" >Learn</Link>
+            <Link href={'#'} className="text-lg font-medium hover:opacity-75" >Products</Link>
+            <Link href={'#'} className="text-lg font-medium hover:opacity-75" >Customers</Link>
+            <Link href={'#'} className="text-lg font-medium hover:opacity-75" >Pricing</Link>
+            <Link href={'#'} className="text-lg font-medium hover:opacity-75" >Learn</Link>
         </nav>
     )
 }
