22029cb5c31719dea80d7c3b32bd314e18594f22
Ehsan <ehsan.yaghoti@yahoo.com>
2025-12-04 02:15:16 +0330

    responsive steps

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -9,17 +9,17 @@ const Steps = () => {
     const [containerRef , isVisible] = useElementObserver<HTMLDivElement>({root : null , rootMargin : "0px" , threshold : 0.2}) 
 
     return (
-        <div ref={containerRef} className=" w-full p-20 bg-accent text-white flex flex-col gap-12 overflow-hidden" >
+        <div ref={containerRef} className=" w-full p-6 lg:p-20 bg-accent text-white flex flex-col gap-12 overflow-hidden" >
             <div className="flex flex-col gap-3 " >
                 <span className={`${isVisible ? "opacity-100" : "opacity-0"} duration-1000 ease-out transition-all text-xs text-primary uppercase font-medium`} >Step</span>
                 <h2 className={
                     `${isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10 "} duration-1000 delay-200 ease-out transition-all
-                    text-3xl  leading-10`
+                    text-3xl  leading-10 lg:w-[500px]`
                 }>
-                    Maximize your returns with a <br /> Reserve account that generates.
+                    Maximize your returns with a Reserve account that generates.
                 </h2>
             </div>
-            <div className="flex gap-8 justify-between  " >
+            <div className="flex flex-col  md:flex-row gap-8 justify-between  " >
                 <BigNumCard isVisible={isVisible} data={{ number: 1, title: "Open your account", description: "Sign up to finpay and set up your account from the dashboard." }} delay={400} />
                 <BigNumCard isVisible={isVisible} data={{ number: 2, title: "Transfer your money", description: "Move money from to another account into and start to earning up." }} delay={1000} />
                 <BigNumCard isVisible={isVisible} data={{ number: 3, title: "Watch your balance grow", description: "Accessed instantlt and remain insulated from market volatility." }} delay={1600} />
